9f7b95b4972ae93c4fd2e59dc645b1546bf0fe1f
Promode <pramoddutta_96@yahoo.com>
2025-11-18 08:04:26 +0530

    18th Nove

--- task/Task_TriaglePrinting.py
+++ task/Task_TriaglePrinting.py
@@ -1,11 +1,14 @@
-# *
-# * *
-# * * *
-# n = 3
+# # *
+# # * *
+# # * * *
+# # n = 3
+#
+# rows = int(input("Enter the rows for the Right Angle Triangle"))
+#
+# for i in range(1, rows + 1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
 
-rows = int(input("Enter the rows for the Right Angle Triangle"))
-
-for i in range(1, rows + 1):
-    for j in range(i):
-        print("*",end="")
-    print()
+for i in range(1, 6):
+    print("*" * i)
